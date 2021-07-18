b5def7b015ac80a3994246ad0d2296136575acb7
Xeeynamo <Xeeynamo@users.noreply.github.com>
2021-07-18 11:57:28 +0000

    Deploying to gh-pages from  @ 198285aeb7306f8d58830377ef439a93620f4adc 🚀

---
+++ precache-manifest.e0bb9747a32a21a19688b8234bdcae42.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0c9ed83d41f7f44663702246cd98f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ea8b54fa826edc357b0",
+    "url": "/static/css/3.4b02a5f2.chunk.css"
+  },
+  {
+    "revision": "cb055b068b4a8a9ce772",
+    "url": "/static/css/4.7bc24cb5.chunk.css"
+  },
+  {
+    "revision": "078b9ea2a7f48cd98b61",
+    "url": "/static/css/main.f6ce7fed.chunk.css"
+  },
+  {
+    "revision": "eb1c3945404f06164728",
+    "url": "/static/js/2.5abaa413.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.5abaa413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea8b54fa826edc357b0",
+    "url": "/static/js/3.7e29961b.chunk.js"
+  },
+  {
+    "revision": "cb055b068b4a8a9ce772",
+    "url": "/static/js/4.e938f305.chunk.js"
+  },
+  {
+    "revision": "078b9ea2a7f48cd98b61",
+    "url": "/static/js/main.d21cec4e.chunk.js"
+  },
+  {
+    "revision": "4f657b2f5b5a5417a32c",
+    "url": "/static/js/runtime-main.fba9aa3c.js"
+  },
+  {
+    "revision": "6baf298a10caf46497589ec351676b58",
+    "url": "/static/media/7digital.6baf298a.png"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "527c95037098f5b386795256e5ec8272",
+    "url": "/static/media/edilportale.527c9503.webp"
+  },
+  {
+    "revision": "6d976ec6319b09da0055e4e9c78c53c7",
+    "url": "/static/media/imaginecup16.6d976ec6.jpg"
+  },
+  {
+    "revision": "d2007c9ebd8387bdba698bc2bc0e9db7",
+    "url": "/static/media/kingdomsaveeditor.d2007c9e.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "2055b126b3237a4aa0dc7c9a64d5fdbf",
+    "url": "/static/media/megamanx-logo.2055b126.png"
+  },
+  {
+    "revision": "ce48b2a8d2cbd78eafbed8b5a7323add",
+    "url": "/static/media/modenanerd17.ce48b2a8.jpg"
+  },
+  {
+    "revision": "e24b0bd2c304b1f30da5cb1728c649e5",
+    "url": "/static/media/nearcons.e24b0bd2.png"
+  },
+  {
+    "revision": "0bfc032cc05c7374e27ce26bf972dd87",
+    "url": "/static/media/openkh.0bfc032c.png"
+  },
+  {
+    "revision": "902be17388476c476d138e016e76bb86",
+    "url": "/static/media/safetonet.902be173.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "1f8f0532a9d5c5dc2925104cf3338428",
+    "url": "/static/media/soc.1f8f0532.png"
+  },
+  {
+    "revision": "45b1e3d6e4e2bf9fcb0e6f402bcbe447",
+    "url": "/static/media/sonic1.45b1e3d6.png"
+  },
+  {
+    "revision": "7588db3076f4abb5aa7812b22972124c",
+    "url": "/static/media/sonicmania.7588db30.png"
+  },
+  {
+    "revision": "a8362988336a1872fc73cf2d0cb9a083",
+    "url": "/static/media/svilupparty17.a8362988.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "c8a1c675402375311bafe57f9828ee10",
+    "url": "/static/media/uls.c8a1c675.png"
+  },
+  {
+    "revision": "0a5aba11465737c620dfde2df0280cd4",
+    "url": "/static/media/unity3d.0a5aba11.png"
+  },
+  {
+    "revision": "f701311d0f20bf4957807e8126390cfd",
+    "url": "/static/media/xeengine.f701311d.png"
+  }
+]);
