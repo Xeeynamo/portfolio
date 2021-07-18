9e6a85004e3b6ebc3eb8a53bc6576413dcbad551
Luciano Ciccariello <xeeynamo@hotmail.com>
2021-07-18 12:33:01 +0100

    Integrate views from the Contact component

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./GithubProfileCard.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import {contactInfo} from "../../portfolio";
 import emoji from "react-easy-emoji";
 import {Fade} from "react-reveal";
+import StyleContext from "../../contexts/StyleContext";
 
 export default function GithubProfileCard({prof}) {
+  const {isDark} = useContext(StyleContext);
   if (prof.isHireable) {
     prof.hireable = "Yes";
   } else {
@@ -14,13 +16,23 @@ export default function GithubProfileCard({prof}) {
   return (
     <Fade bottom duration={1000} distance="20px">
       <div className="main" id="contact">
-        <h1 className="prof-title">Reach Out to me!</h1>
+        <h1 className="heading contact-title">{contactInfo.title}</h1>
         <div className="row">
           <div className="main-content-profile">
             <div className="blog-header">
-              <p className="subTitle blog-subtitle">{contactInfo.subtitle}</p>
+              <p
+                className={
+                  isDark
+                    ? "dark-mode contact-subtitle"
+                    : "subTitle contact-subtitle"
+                }
+              >
+                {contactInfo.subtitle}
+              </p>
             </div>
-            <h2 className="bio-text">"{emoji(String(prof.bio))}"</h2>
+            {prof.bio ? (
+              <h2 className="bio-text">"{emoji(String(prof.bio))}"</h2>
+            ) : null}
             {prof.location !== null && (
               <div className="location-div">
                 <span className="desc-prof">
@@ -45,6 +57,32 @@ export default function GithubProfileCard({prof}) {
               <span className="desc-prof">
                 Open for opportunities: {prof.hireable}
               </span>
+            </div>
+            <div
+              className={
+                isDark ? "dark-mode contact-text-div" : "contact-text-div"
+              }
+            >
+              {contactInfo.number ? (
+                <div>
+                  <a
+                    className="contact-detail"
+                    href={"tel:" + contactInfo.number}
+                  >
+                    {contactInfo.number}
+                  </a>
+                  <br />
+                  <br />
+                </div>
+              ) : null}
+              <a
+                className="contact-detail-email"
+                href={"mailto:" + contactInfo.email_address}
+              >
+                {contactInfo.email_address}
+              </a>
+              <br />
+              <br />
             </div>
             <SocialMedia />
           </div>
