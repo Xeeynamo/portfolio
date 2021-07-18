a446463b48b7ec9c339731adcbc73365fb900b14
Luciano Ciccariello <xeeynamo@hotmail.com>
2021-07-18 13:20:28 +0100

    Make prettier happier

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,9 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Saad Pasta, customised by Luciano Ciccariello 🧑‍💻")}
+          {emoji(
+            "Made with ❤️ by Saad Pasta, customised by Luciano Ciccariello 🧑‍💻"
+          )}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
